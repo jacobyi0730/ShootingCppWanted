63606a12d80810d44d3325e0a7fea795508a1ac8
f11m4at4 <jacobyi0730@gmail.com>
2025-01-14 12:25:56 +0900

    20250114
    DestroyZone
    주인공과 적의 외형 교체

--- Source/ShootingCppWanted/Public/DestroyActor.h
+++ Source/ShootingCppWanted/Public/DestroyActor.h
@@ -24,16 +24,16 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	// 충돌체와 외관을 만들고 싶다.
+	// 충돌체의 충돌프로필은 "DestroyZone"
+	// 충돌처리 함수도 만들고 충돌체의 OnComponentBeginOverlap에 연결하고싶다.
+	// 누군가와 부딪히면 파괴하고싶다.
+public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	class UBoxComponent* Root;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	class UStaticMeshComponent* Mesh;
-	// 충돌체의 충돌프로필은 "DestroyZone"
-	// 충돌처리 함수도 만들고 충돌체의 OnComponentBeginOverlap에 연결하고싶다.
 	UFUNCTION()
 	void OnMyBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
-	
-	// 누군가와 부딪히면 파괴하고싶다.
 
 };
